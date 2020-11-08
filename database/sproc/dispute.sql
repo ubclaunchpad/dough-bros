e44099b345734fa115d21453fc9107a2ce6ef861
Andrei <isabelle.andre14@hotmail.com>
2020-11-08 14:59:19 -0800

    added dispute sprocs

---
+++ database/sproc/dispute.sql
@@ -0,0 +1,29 @@
+#
+# STORED PROCEDURES FOR PAYMENT DISPUTES
+#
+
+USE `doughBros_db`;
+DROP procedure IF EXISTS `createDispute`;
+DROP procedure IF EXISTS `resolveDispute`;
+DELIMITER $$
+USE `doughBros_db`$$
+CREATE PROCEDURE `createDispute` (IN `dispute_id` INT(8), IN `initiator_id` INT(8), IN `recipient_id` INT(8), IN `payment_id` INT(8), IN `type` VARCHAR(255), IN `description` VARCHAR(255))
+BEGIN
+
+INSERT INTO `dispute` (`fk_creator_id`, `fk_initiator_id`, `fk_recipient_id`, `fk_recipient_id`, `dispute_type`, `dispute_description`)
+	VALUES (`dispute_id`, `initiator_id`, `recipient_id`, `recipient_id`, `type`, `description`);
+
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+USE `doughBros_db`$$
+CREATE PROCEDURE `resolveDispute` (IN `dispute_id` INT(8))
+BEGIN
+
+DELETE FROM `dispute` WHERE `dispute_id` = `dispute_id`;
+
+END$$
+
+DELIMITER ;
--- database/sproc/user.sql
+++ database/sproc/user.sql
@@ -4,6 +4,7 @@
 
 USE `doughBros_db`;
 DROP procedure IF EXISTS `createUser`;
+DROP procedure IF EXISTS `getGroupsByUser`;
 
 DELIMITER $$
 USE `doughBros_db`$$
@@ -12,17 +13,6 @@ BEGIN
 
 INSERT INTO `user` (`first_name`, `last_name`, `email_addr`, `facebook_id`, `is_anon`)
 	VALUES (`first_name`, `last_name`, `email_addr`, `facebook_id`, `is_anon`);
-
-END$$
-
-DELIMITER ;
-
-DELIMITER $$
-USE `doughBros_db`$$
-CREATE PROCEDURE `getUsers` (IN `user_id` INT(8))
-BEGIN
-
-SELECT * FROM `user` WHERE `user_id` = `user_id`;
 
 END$$
 
