f7f74951ef0fb88e242d08579861ca881689b81f
Wren Liang <wren.liang@gmail.com>
2020-11-08 18:40:34 -0700

    restructured Alice's tables into new format, and fixed node mysql connection issue

--- server/src/config/databaseHandler.ts
+++ server/src/config/databaseHandler.ts
@@ -1,19 +1,37 @@
 "use strict";
 
-var mysql = require("mysql");
+const mysql = require("mysql");
+const util = require("util");
 
 const config = {
-  host: "localhost",
-  port: 3307,
-  user: "user",
+  host: "doughBros_db",
+  port: 3306,
+  user: "root",
   password: "root",
   database: "doughBros_db"
 };
 
-const connection = mysql.createConnection(config);
-connection.connect(function(err:any) {
-  if (err) console.log(err);
-  console.log("Database Connected: " + config.host + ":" + config.port);
+const connectionPool = mysql.createPool(config);
+
+// Test Call
+connectionPool.getConnection((err: any, connection: any) => {
+  if (err) {
+      console.error(err);
+  } else {
+      console.log(`[MySQL] Successful connection: ${config.host}:${config.port}`);
+  }
+
+  if (connection) {
+      connection.release();
+  }
 });
 
-module.exports = connection;
+connectionPool.query = util.promisify(connectionPool.query);
+
+// const connection = mysql.createConnection(config);
+// connection.connect(function(err:any) {
+//   if (err) console.log(err);
+//   console.log("Database Connected: " + config.host + ":" + config.port);
+// });
+
+module.exports = connectionPool;
