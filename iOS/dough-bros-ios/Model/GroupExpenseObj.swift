6d4814314f041f69b21357a541e6146963e196ca
Andrei <isabelle.andre14@hotmail.com>
2021-03-02 22:16:16 -0800

    group expense model, amount double type

--- iOS/dough-bros-ios/Model/GroupExpenseObj.swift
+++ iOS/dough-bros-ios/Model/GroupExpenseObj.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+typealias user_tuple = (fk_sender_id: String, amount: Double)
+
 struct GroupExpenseObj: Codable {
-    
+    var fk_gorup_id: Int
+    var fk_added_by_id: String
+    var fk_currency_id: Int
+    var expense_name: String
+    var is_archived: Int
+    var amount: Double
+    var user_amounts: [user_tuple] = user_tuple[]()
 }
